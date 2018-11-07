380ea790df00eff0340e966b6596342431c50361
Artyom Mordvinov <1992bress@gmail.com>
2018-11-08 00:45:06 +0300

    login& register logics

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ let mongoose = require('mongoose');
 let bodyParser = require('body-parser');
 let emailValidate = require('./modules/emailvalidate.js');
 let User = require('./schemas/userSchema.js');
+let cors = require('cors');
+
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -12,8 +16,10 @@ mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("we are connected to DB!")
+  console.log("we are connected to DB!");
+  
 });
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
@@ -23,15 +29,13 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/simplehtml.html');
 });
 
-app.post('/', function (req, res) {
+app.post('/register', function (req, res) {
   let postedForm = req.body,
     emailCorrect = false,
     passwConf = false;
-    usernameIsFree = false;
     emailIsFree = false;
     passIsEmpty = true;
-    console.log(req);
-    console.log(postedForm);
+    console.log(req.body);
   emailValidate(postedForm.email) ? emailCorrect = true : emailCorrect = false;
   postedForm.password === postedForm.passwordConf ? passwConf = true : passwConf  = false;
   postedForm.password === "" ? passIsEmpty = true : passIsEmpty = false;
@@ -45,10 +49,13 @@ app.post('/', function (req, res) {
     submitteduser.save(function (err) {
       if (err) 
       {
-        res.end('Seems like email or username is currently in use!');    
         
+        res.status(200).json({ message: 'Seems like email or username is currently in use!'});
+            
       } else {
-        res.end('Successfully saved!');
+        
+        res.status(200).json({ message: 'Successfully saved!'});
+        
       };
       });
     
@@ -64,7 +71,30 @@ app.post('/', function (req, res) {
       resultError = resultError + "You are easy victim! Your password is empty!\n"
     }
     
-    res.end(resultError);
+    res.status(200).json({ message: resultError});
+    
   }
 }); 
 
+app.post('/login', function (req, res) {
+  let postedForm = req.body;
+  console.log(req.body);
+  User.findOne({username: postedForm.name}, function (err, user) {
+    if (user) {
+      if  (user.checkPassword(postedForm.password)) {
+        
+        res.status(200).json({ message: "Access granted"});
+      
+      } else {
+        
+        res.status(200).json({ message: "Access denied. Check username and pass."});
+      
+    }
+    } else {
+      
+      res.status(200).json({ message: "Access denied. Check username and pass."});
+      
+    }
+  });
+  }
+); 
