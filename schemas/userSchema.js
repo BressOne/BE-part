579e6f9fe56637338cacd2b6aa15eef2355a6153
Artyom Mordvinov <1992bress@gmail.com>
2018-11-08 14:41:58 +0300

    prettyfied

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,43 +1,46 @@
-let mongoose = require('mongoose');
-let crypto = require('crypto');
-let UserSchema = new mongoose.Schema ({
-    email: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-      },
-      username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true
-      },
-      hashedPassword: {
-      type: String,
-      required: true 
-      },
-      salt: {
-        type: String,
-        required: true,
-      }
-      
+let mongoose = require("mongoose");
+let crypto = require("crypto");
+let UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  hashedPassword: {
+    type: String,
+    required: true
+  },
+  salt: {
+    type: String,
+    required: true
+  }
 });
 
-UserSchema.methods.encryptPassword = function (password) {
-return crypto.createHmac('sha1', this.salt).update(password).digest('hex');
+UserSchema.methods.encryptPassword = function(password) {
+  return crypto
+    .createHmac("sha1", this.salt)
+    .update(password)
+    .digest("hex");
 };
 
-UserSchema.virtual('password').set(function (password) {
-  this._plainPassword = password,
-  this.salt = Math.random() + '';
+UserSchema.virtual("password").set(function(password) {
+  (this._plainPassword = password), (this.salt = Math.random() + "");
   this.hashedPassword = this.encryptPassword(password);
 });
 
-UserSchema.virtual('password').get(function() {return this._plainPassword;});
-UserSchema.methods.checkPassword = function (password) {
+UserSchema.virtual("password").get(function() {
+  return this._plainPassword;
+});
+UserSchema.methods.checkPassword = function(password) {
   return this.encryptPassword(password) === this.hashedPassword;
-}
+};
 
-let User = mongoose.model('User', UserSchema);
+let User = mongoose.model("User", UserSchema);
 module.exports = User;
--- server.js
+++ server.js
@@ -1,100 +1,99 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-let mongoose = require('mongoose');
-let bodyParser = require('body-parser');
-let emailValidate = require('./modules/emailvalidate.js');
-let User = require('./schemas/userSchema.js');
-let cors = require('cors');
-
+let mongoose = require("mongoose");
+let bodyParser = require("body-parser");
+let emailValidate = require("./modules/emailvalidate.js");
+let User = require("./schemas/userSchema.js");
+let cors = require("cors");
 
 app.use(cors());
+app.options("*", cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+mongoose.connect(
+  "mongodb://localhost/test",
+  { useNewUrlParser: true }
+);
 let db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function() {
   console.log("we are connected to DB!");
-  
 });
 
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/simplehtml.html');
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/simplehtml.html");
 });
 
-app.post('/register', function (req, res) {
+app.post("/register", function(req, res) {
   let postedForm = req.body,
     emailCorrect = false,
     passwConf = false;
-    emailIsFree = false;
-    passIsEmpty = true;
-    console.log(req.body);
-  emailValidate(postedForm.email) ? emailCorrect = true : emailCorrect = false;
-  postedForm.password === postedForm.passwordConf ? passwConf = true : passwConf  = false;
-  postedForm.password === "" ? passIsEmpty = true : passIsEmpty = false;
+  emailIsFree = false;
+  passIsEmpty = true;
+  console.log(req.body);
+  emailValidate(postedForm.email)
+    ? (emailCorrect = true)
+    : (emailCorrect = false);
+  postedForm.password === postedForm.passwordConf
+    ? (passwConf = true)
+    : (passwConf = false);
+  postedForm.password === "" ? (passIsEmpty = true) : (passIsEmpty = false);
 
-  if (emailCorrect &&  passwConf && !passIsEmpty) {
-    let submitteduser = new User ({
+  if (emailCorrect && passwConf && !passIsEmpty) {
+    let submitteduser = new User({
       email: postedForm.email,
       username: postedForm.username,
       password: postedForm.password
     });
-    submitteduser.save(function (err) {
-      if (err) 
-      {
-        
-        res.status(200).json({ message: 'Seems like email or username is currently in use!'});
-            
+    submitteduser.save(function(err) {
+      if (err) {
+        res.status(200).json({
+          message: "Seems like email or username is currently in use!"
+        });
       } else {
-        
-        res.status(200).json({ message: 'Successfully saved!'});
-        
-      };
-      });
-    
+        res.status(200).json({ message: "Successfully saved!" });
+      }
+    });
   } else {
     let resultError = "";
     if (!emailCorrect) {
-      resultError = resultError + "Keep yourself together! Your e-mail is incorrect\n"
+      resultError =
+        resultError + "Keep yourself together! Your e-mail is incorrect\n";
     }
     if (!passwConf) {
-      resultError = resultError + "Keep up! Your passwords are not same!\n"
+      resultError = resultError + "Keep up! Your passwords are not same!\n";
     }
     if (passIsEmpty) {
-      resultError = resultError + "You are easy victim! Your password is empty!\n"
+      resultError =
+        resultError + "You are easy victim! Your password is empty!\n";
     }
-    
-    res.status(200).json({ message: resultError});
-    
-  }
-}); 
 
-app.post('/login', function (req, res) {
+    res.status(200).json({ message: resultError });
+  }
+});
+
+app.post("/login", function(req, res) {
   let postedForm = req.body;
   console.log(req.body);
-  User.findOne({username: postedForm.name}, function (err, user) {
+  User.findOne({ username: postedForm.name }, function(err, user) {
     if (user) {
-      if  (user.checkPassword(postedForm.password)) {
-        
-        res.status(200).json({ message: "Access granted"});
-      
+      if (user.checkPassword(postedForm.password)) {
+        res.status(200).json({ message: "Access granted" });
       } else {
-        
-        res.status(200).json({ message: "Access denied. Check username and pass."});
-      
-    }
+        res
+          .status(200)
+          .json({ message: "Access denied. Check username and pass." });
+      }
     } else {
-      
-      res.status(200).json({ message: "Access denied. Check username and pass."});
-      
+      res
+        .status(200)
+        .json({ message: "Access denied. Check username and pass." });
     }
   });
-  }
-); 
+});
