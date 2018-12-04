1e362e489cd7c60bbcdeb6e5f85b6b3a5267802e
Artyom Mordvinov <1992bress@gmail.com>
2018-12-05 01:09:35 +0300

    cookis workout

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -1,6 +1,10 @@
 let mongoose = require("mongoose");
-let crypto = require("crypto");
+// let crypto = require("crypto");
+let passportLocalMongoose = require("passport-local-mongoose");
+
 let UserSchema = new mongoose.Schema({
+  // username: String,
+  // password: String
   email: {
     type: String,
     unique: true,
@@ -10,37 +14,37 @@ let UserSchema = new mongoose.Schema({
   username: {
     type: String,
     unique: true,
-    required: true,
+    // required: true,
     trim: true
   },
-  hashedPassword: {
-    type: String,
-    required: true
-  },
-  salt: {
-    type: String,
-    required: true
+  password: {
+    type: String
+    // required: true
   }
+  // salt: {
+  //   type: String,
+  //   required: true
 });
 
-UserSchema.methods.encryptPassword = function(password) {
-  return crypto
-    .createHmac("sha1", this.salt)
-    .update(password)
-    .digest("hex");
-};
+UserSchema.plugin(passportLocalMongoose);
 
-UserSchema.virtual("password").set(function(password) {
-  (this._plainPassword = password), (this.salt = Math.random() + "");
-  this.hashedPassword = this.encryptPassword(password);
-});
+// UserSchema.methods.encryptPassword = function(password) {
+//   return crypto
+//     .createHmac("sha1", this.salt)
+//     .update(password)
+//     .digest("hex");
+// };
 
-UserSchema.virtual("password").get(function() {
-  return this._plainPassword;
-});
-UserSchema.methods.checkPassword = function(password) {
-  return this.encryptPassword(password) === this.hashedPassword;
-};
+// UserSchema.virtual("password").set(function(password) {
+//   (this._plainPassword = password), (this.salt = Math.random() + "");
+//   this.hashedPassword = this.encryptPassword(password);
+// });
 
-let User = mongoose.model("User", UserSchema);
-module.exports = User;
+// UserSchema.virtual("password").get(function() {
+//   return this._plainPassword;
+// });
+// UserSchema.methods.checkPassword = function(password) {
+//   return this.encryptPassword(password) === this.hashedPassword;
+// };
+
+module.exports = mongoose.model("User", UserSchema);
--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 let express = require("express");
 let app = express();
+
 let mongoose = require("mongoose");
-let bodyParser = require("body-parser");
+
 let emailValidate = require("./modules/emailvalidate.js");
-let User = require("./schemas/userSchema.js");
 let cors = require("cors");
 
+let path = require("path");
+
+let bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
 app.use(cors());
+
 app.options("*", cors());
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
@@ -19,31 +24,66 @@ app.use(function (req, res, next) {
   next();
 });
 
+// let morgan = require("morgan");
+
+let methodOverride = require("method-override");
+app.use(methodOverride());
+
+let cookieParser = require("cookie-parser");
+app.use(cookieParser("scary_secret"));
+
+// app.use(express.session());
+let session = require("express-session");
+app.use(
+  session({
+    secret: "work hard",
+    resave: true,
+    saveUninitialized: false
+  })
+);
+
+let passport = require("passport");
+let LocalStrategy = require("passport-local").Strategy;
+app.use(passport.initialize());
+app.use(passport.session());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, "public")));
+
+// passport config
+let User = require("./schemas/userSchema.js");
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// routes
+// require("./routes")(app);
+
 mongoose.connect(
-  "mongodb://localhost/test",
+  "mongodb://localhost/passportDB",
   { useNewUrlParser: true }
 );
 let db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
+db.once("open", function() {
   console.log("we are connected to DB!");
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.listen(3000, function() {
+  console.log("listening on port 3000!");
 });
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/simplehtml.html");
-});
+// app.get("/", function(req, res) {
+//   res.sendFile(__dirname + "/simplehtml.html");
+// });
 
-app.post("/register", cors(), function (req, res) {
+app.post("/register", cors(), function(req, res) {
   let postedForm = req.body,
     emailCorrect = false,
     passwConf = false;
   emailIsFree = false;
   passIsEmpty = true;
   console.log(req.body);
+
   emailValidate(postedForm.email)
     ? (emailCorrect = true)
     : (emailCorrect = false);
@@ -53,20 +93,24 @@ app.post("/register", cors(), function (req, res) {
   postedForm.password === "" ? (passIsEmpty = true) : (passIsEmpty = false);
 
   if (emailCorrect && passwConf && !passIsEmpty) {
-    let submitteduser = new User({
-      email: postedForm.email,
-      username: postedForm.username,
-      password: postedForm.password
-    });
-    submitteduser.save(function (err) {
-      if (err) {
-        res.json({
-          message: "Seems like email or username is currently in use!"
+    console.log("all is correct and validated");
+    User.register(
+      new User({ email: postedForm.email, username: postedForm.username }),
+      postedForm.password,
+      function(err, account) {
+        if (err) {
+          res.json({
+            message: "Seems like email or username is currently in use!"
+          });
+        }
+        console.log(err);
+        passport.authenticate("local")(req, res, function() {
+          res.json({
+            message: "Saved!"
+          });
         });
-      } else {
-        res.json({ message: "Successfully saved!" });
       }
-    });
+    );
   } else {
     let resultError = "";
     if (!emailCorrect) {
@@ -85,44 +129,54 @@ app.post("/register", cors(), function (req, res) {
   }
 });
 
-app.post("/login", function (req, res) {
-  let postedForm = req.body;
+app.post("/login", passport.authenticate("local"), function(req, res) {
   console.log(req.body);
-  User.findOne({ username: postedForm.loginusername }, function (err, user) {
-    console.log(user);
-    if (user) {
-      if (user.checkPassword(postedForm.loginpassword)) {
-        res.status(200).json({
-          message: "Access granted",
-          loginPermission: true
-        });
-      } else {
-        res.status(200).json({
-          message: "Wrong username or pass.",
-          loginPermission: false
-        });
-      }
-    } else {
-      res.status(200).json({
-        message: "Wrong username or pass.",
-        loginPermission: false
-      });
-    }
+  res.status(200).json({
+    message: "Access granted",
+    loginPermission: true
   });
 });
 
-app.post("/search_person", function (req, res) {
+// app.post("/login", function(req, res) {
+//   let postedForm = req.body;
+//   console.log(req.body);
+//   User.findOne({ username: postedForm.loginusername }, function(err, user) {
+//     console.log(user);
+//     if (user) {
+//       if (user.checkPassword(postedForm.loginpassword)) {
+//         res.status(200).json({
+//           message: "Access granted",
+//           loginPermission: true
+//         });
+//       } else {
+//         res.status(200).json({
+//           message: "Wrong username or pass.",
+//           loginPermission: false
+//         });
+//       }
+//     } else {
+//       res.status(200).json({
+//         message: "Wrong username or pass.",
+//         loginPermission: false
+//       });
+//     }
+//   });
+// });
+
+app.post("/search_person", function(req, res) {
   console.log(req);
 
   console.log(req.body.searchValue);
   const pattern = req.body.searchValue;
   console.log(pattern);
-  User.find({ username: { $regex: `${pattern}`, $options: 'i' } }, function (err, users) {
+  User.find({ username: { $regex: `${pattern}`, $options: "i" } }, function(
+    err,
+    users
+  ) {
     console.log(users);
     let resultList = [];
     for (let index = 0; index < users.length; index++) {
-      resultList.push(users[index].username)
-
+      resultList.push(users[index].username);
     }
     console.log(resultList);
     res.status(200).json({
