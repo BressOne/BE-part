f5ba63fbd4d087f939d6ae0038ad5da50737f2cd
Artyom Mordvinov <1992bress@gmail.com>
2019-01-09 19:18:33 +0300

    cprs

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 let corsOptions = {
-  origin: true,
+  origin: "https://bressone.github.io/FE-part/",
   credentials: true,
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: true,
