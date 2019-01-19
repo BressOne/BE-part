d0453a6c80fd4b6cc7ca53f2171ad3ec5c2ffe39
1992bress@gmail.com <1992bress@gmail.com>
2019-01-19 10:38:35 +0300

    port adaptation for heroku publishing

--- server.js
+++ server.js
@@ -164,7 +164,8 @@ io.on("connection", function(socket) {
   });
 });
 
-io.listen(8000);
+io.listen(process.env.PORT || 8000, () => {console.log("SIO listening on port " + process.env.PORT || 8000)});
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -185,5 +186,5 @@ app.use((err, req, res, next) => {
 
 app.listen(process.env.PORT || 3000, () => {
 //app.listen(3000, () => {
-  console.log("Chat app listening on port " + process.env.PORT);
+  console.log("Chat app listening on port " + process.env.PORT || 3000);
 });
